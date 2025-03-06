5a88152604b707c3c58cfc9ea745fbc4592ffe49
NiYufei <nyfyfn@163.com>
2025-03-06 23:50:49 +0800

    update config

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 # backend/config.py
-DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/mydb"
-SECRET_KEY = "your-secret-key"
+DATABASE_URL = "postgresql+asyncpg://postgres:ZAQ!xsw2cde3@localhost:5432/mydb"
+SECRET_KEY = "51331b12719e4c9308b7c23b1684e175227199d775ad4f87bd84c842075825a8"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 PORT = 8000
